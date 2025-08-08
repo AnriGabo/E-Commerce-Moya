eec57bf28fe1d9eac0ca3e46206919f77e7cb880
Anri <@anrigabaidze44@gmail.com>
2025-08-08 04:46:10 +0400

    feat: add 4-video slider component with navigation arrows

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,1 +1,14 @@
 // ეს იქნება რეალურად ლენდინგ ფეიჯი, პირველი იუზერის გამოცდილება
+
+import { Stack } from "@mui/material";
+import VideoSlider from "../../Components/Home/VideoSlider";
+
+const HomePage = () => {
+  return (
+    <Stack>
+      <VideoSlider />
+    </Stack>
+  );
+};
+
+export default HomePage;
