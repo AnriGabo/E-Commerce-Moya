fe710d65d3406fd7174581c9fb4c9e9b2eaa4b16
Anri <anrigabaidze44@gmail.com>
2025-08-10 20:38:30 +0400

    restructured file

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 
 import { Stack } from "@mui/material";
-import VideoSlider from "../../Components/Home/VideoSlider";
+import VideoSlider from "../../Components/VideoCarousel/VideoSlider";
+import Collection from "../../Components/PhotoGallery/Collection";
 
 const HomePage = () => {
   return (
     <Stack>
       <VideoSlider />
+      <Collection />
     </Stack>
   );
 };
