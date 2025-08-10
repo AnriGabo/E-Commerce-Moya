14d23f15c22d65db6984b0c0f577858d7676aa8a
Anri <anrigabaidze44@gmail.com>
2025-08-10 20:49:19 +0400

    I added a login registration file

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,8 +1,8 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import { Helmet } from "react-helmet";
 
 // ეს იქნება რეალურად ჰედერი რომელსაც აპში გამოვიყენებთ რადგან ყველგან ჩანდეს
-import BurgerMenu from "./BurgerMenu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 export default function Header() {
   return (
@@ -18,7 +18,62 @@ export default function Header() {
           flexDirection: "row",
         }}
       >
-         <BurgerMenu />
+        <Stack
+          sx={{
+            flex: "1",
+            flexDirection: "row",
+            alignItems: "center",
+            gap: "1rem",
+          }}
+        >
+          <Box>
+            <MenuOpenIcon
+              sx={{
+                cursor: "pointer",
+                fontSize: "3rem",
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography
+              variant={"h1"}
+              component={"h2"}
+              sx={{
+                fontSize: "7rem",
+                cursor: "pointer",
+                fontFamily: "'Libre Baskerville', serif",
+                fontWeight: 700,
+                letterSpacing: "1px",
+              }}
+            >
+              MOYA
+            </Typography>
+          </Box>
+        </Stack>
+
+        <Stack
+          sx={{
+            flexDirection: "row",
+            justifyContent: "end",
+            paddingInlineEnd: "1rem",
+            gap: "1rem",
+            marginBlockEnd: "2rem",
+            alignItems: "center",
+            // backgroundColor:"blue"
+          }}
+        >
+          
+          <Box>
+            <Button sx={{ color: "red", textTransform: "capitalize" }}>
+              Sign In
+            </Button>
+          </Box>
+          <Box>
+            <Button sx={{ color: "red", textTransform: "capitalize" }}>
+              Sign Up
+            </Button>
+          </Box>
+        </Stack>
       </Stack>
     </Box>
   );
