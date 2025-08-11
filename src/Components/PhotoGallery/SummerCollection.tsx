35b4f20f3d0a1b8aa6f5ff6add0e842ae36e728d
Anri <anrigabaidze44@gmail.com>
2025-08-11 16:44:47 +0400

    Update: spring and summer collection component, replace images, changes title position

--- src/Components/PhotoGallery/SummerCollection.tsx
+++ src/Components/PhotoGallery/SummerCollection.tsx
@@ -1,32 +1,40 @@
 import { Box, Stack, Typography } from "@mui/material";
+import SummerCollectionCloth from "../../Images/FashionModel0.jpg";
+import SummerCollectionCloth2 from "../../Images/FashionModel1.jpg";
+import SummerCollectionCloth3 from "../../Images/FashionModel2.jpg";
 
 const summerCollectionImg = [
   {
-    img: "/src/Images/ModelViewN5.jpg",
+    img: SummerCollectionCloth,
   },
   {
-    img: "/src/Images/ModelViewN6.jpg",
+    img: SummerCollectionCloth2,
   },
   {
-    img: "/src/Images/ModelView7.jpg",
+    img: SummerCollectionCloth3,
   },
   {
-    h5: "002",
-    h6: "summer,woman collection",
+    h4: "002",
+    h5: "summer,woman",
+    h6: "collection",
   },
 ];
 
 const SummerCollection = () => {
   return (
-    <Stack sx={{ marginBlockStart: "5rem" }}>
+    <Stack sx={{ marginBlockStart: "8rem" }}>
       <Box sx={{ display: "grid", gridTemplateColumns: "repeat(2,1fr)" }}>
         {summerCollectionImg.map((Item, I) => (
           <Stack>
-            <img src={Item.img} key={I} style={{ width: "100%" }} />
+            <img
+              src={Item.img}
+              key={I}
+              style={{ width: "100%", cursor: "pointer" }}
+            />
             <Box
               sx={{
                 display: "flex",
-                justifyContent: "center",
+                justifyContent: "end",
                 alignItems: "center",
                 height: "100%",
                 flexDirection: "column",
@@ -37,16 +45,27 @@ const SummerCollection = () => {
                 component={"h5"}
                 sx={{ fontWeight: "bold" }}
               >
-                {Item.h5}
+                {Item.h4}
               </Typography>
               <Typography
                 sx={{
                   fontFamily: `"Didot", "Bodoni MT", "Palatino Linotype", "Book Antiqua", Palatino, "Garamond", "Georgia", "Times New Roman", Times, serif`,
-                  fontWeight: 400,
+                  fontWeight: 570,
                   letterSpacing: "0.5px",
+                  fontSize: "1.2rem",
                 }}
               >
-                {Item.h6}
+                <i>{Item.h5}</i>
+              </Typography>
+               <Typography
+                sx={{
+                  fontFamily: `"Didot", "Bodoni MT", "Palatino Linotype", "Book Antiqua", Palatino, "Garamond", "Georgia", "Times New Roman", Times, serif`,
+                  fontWeight:570,
+                  letterSpacing: "0.5px",
+                  fontSize: "1.2rem",
+                }}
+              >
+                <i>{Item.h6}</i>
               </Typography>
             </Box>
           </Stack>
