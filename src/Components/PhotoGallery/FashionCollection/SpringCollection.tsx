11121e0c34f2f4cc5637f88e19bf441750a3f14a
Anri <anrigabaidze44@gmail.com>
2025-08-13 04:16:34 +0400

    Seperate Season Collection to New FashionCollection Folder Create Fashion Model component that contains models styles and description Update:Summer and Spring collection images/title position

---
+++ src/Components/PhotoGallery/FashionCollection/SpringCollection.tsx
@@ -0,0 +1,69 @@
+import { Box, Typography } from "@mui/material";
+
+
+import FashionModel3 from "../../../Images/FashionModel3.jpg";
+import FashionModel4 from "../../../Images/FashionModel4.jpg";
+import FashionModel5 from "../../../Images/FashionModel5.jpg";
+import CustomBoxComponent from "../../CustomMuiComponents/CustomBoxComponent";
+import CustomImageDescriptionTypography from "../../CustomMuiComponents/CustomImageDescriptionTypo";
+
+const SpringCollectionGallery = [
+  {
+    img: FashionModel3,
+  },
+  {
+    img: FashionModel4,
+  },
+  {
+    h4: "001",
+    h5: "spring,woman",
+    h6: "collection",
+  },
+  {
+    img: FashionModel5,
+  },
+];
+
+const SpringCollection = () => {
+  return (
+    <CustomBoxComponent>
+      {SpringCollectionGallery.map((Item, i) => (
+        <Box key={i} sx={{ display: "flex", flexDirection: "column" }}>
+          {Item.img && (
+            <img
+              src={Item.img}
+              alt={`Fashion Model Image-${i}`}
+              style={{ width: "100%", objectFit: "cover", cursor: "pointer" }}
+            />
+          )}
+
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "end",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant={"h4"}
+              component={"h5"}
+              sx={{ fontWeight: "bold" }}
+            >
+              {Item.h4}
+            </Typography>
+            <CustomImageDescriptionTypography>
+              <i>{Item.h5}</i>
+            </CustomImageDescriptionTypography>
+            <CustomImageDescriptionTypography>
+              <i>{Item.h6}</i>
+            </CustomImageDescriptionTypography>
+          </Box>
+        </Box>
+      ))}
+    </CustomBoxComponent>
+  );
+};
+
+export default SpringCollection;
---
+++ src/Components/PhotoGallery/FashionModel.tsx
@@ -0,0 +1,95 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+import FashionModel12 from "../../Images/BlackFashionModel3.jpg";
+import FashionModel13 from "../../Images/BlackFashionModel2.jpg";
+import { HelmetProvider } from "react-helmet-async";
+
+const FashionModelStyle = [
+  {
+    img: FashionModel12,
+  },
+  {
+    img: FashionModel13,
+    h4: "LIMITED DROP",
+    h5: "MOYA WOMAN COLLECTION",
+    h6: "BLACK CUT / 003",
+  },
+];
+const FashionModel = () => {
+  return (
+    <Stack
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2,1fr)",
+        marginBlockStart: "8rem",
+      }}
+    >
+      <HelmetProvider>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </HelmetProvider>
+      {FashionModelStyle.map((Item, I) => (
+        <Stack key={I} sx={{ display: "flex", flexDirection: "column" }}>
+          {Item.img && (
+            <img
+              src={Item.img}
+              alt={`Fashion Model Style ${I}`}
+              style={{ width: "100%", objectFit: "cover" }}
+            />
+          )}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "flex-end",
+              flexDirection: "column",
+              paddingBlockStart: "1rem",
+            }}
+          >
+            <Typography
+              component={"h4"}
+              sx={{
+                fontFamily: "'Bodoni Moda', serif",
+                letterSpacing: "2px",
+                fontWeight: 500,
+              }}
+            >
+              {Item.h4}
+            </Typography>
+            <Typography
+              component={"h4"}
+              sx={{
+                letterSpacing: "2px",
+                fontWeight: 500,
+                fontFamily: "Montserrat, sans-serif",
+              }}
+            >
+              {Item.h5}
+            </Typography>
+            <Typography
+              component={"h4"}
+              sx={{
+                letterSpacing: "2px",
+                fontWeight: 500,
+                fontFamily: "Playfair Display, serif",
+              }}
+            >
+              {Item.h6}
+            </Typography>
+          </Box>
+        </Stack>
+      ))}
+    </Stack>
+  );
+};
+
+export default FashionModel;
