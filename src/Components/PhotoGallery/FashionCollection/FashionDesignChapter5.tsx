fe183b98dacd4acfd3fc8b76beba993a8efa613c
Anri <anrigabaidze44@gmail.com>
2025-08-14 18:55:49 +0400

    Add homepage hero and accessories close-up images with descriptive alt text

---
+++ src/Components/PhotoGallery/FashionCollection/FashionDesignChapter5.tsx
@@ -0,0 +1,37 @@
+import { Stack } from "@mui/material";
+import NewChapterDesign from "../../../Images/FashionModelNewChapter0.jpg";
+import FashionDesignChapter6 from "./FashionDesignChapter6";
+
+const Fashion = [
+  {
+    img: NewChapterDesign,
+  },
+];
+
+
+const FashionDesignChapter5 = () => {
+  return (
+    <Stack
+      sx={{
+        marginBlockStart: "8rem",
+        display: "grid",
+        gridTemplateColumns: "repeat(1,1fr)",
+      }}
+    >
+      {Fashion.map((Item, I) => (
+        <Stack key={I}>
+          {Item.img && (
+            <img
+              src={Item.img}
+              alt={`Fashion Model Girl ${I}`}
+              style={{ width: I === 0 ? "100%" : "50%", cursor: "pointer" }}
+            />
+          )}
+        </Stack>
+      ))}
+      <FashionDesignChapter6 />
+    </Stack>
+  );
+};
+
+export default FashionDesignChapter5;
---
+++ src/Components/PhotoGallery/FashionCollection/FashionDesignChapter6.tsx
@@ -0,0 +1,33 @@
+import { Box, Stack, Typography } from "@mui/material";
+import Fashioniko from "../../../Images/FashionModelNewChapter1.jpg";
+
+const FashionDesignChapter6 = () => {
+  return (
+    <Stack
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "repeat(2,1fr)",
+        marginBlockStart: "3rem",
+      }}
+    >
+      <img src={Fashioniko} alt={"Fashion Model"} style={{width:"90%"}}  />
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Typography
+          sx={{
+            fontFamily: '"Didot", "Bodoni MT", "Times New Roman", serif',
+            fontWeight: 400,
+            letterSpacing: "0.4em",
+            fontSize: "2rem",
+            textTransform: "uppercase",
+          }}
+        >
+          ACCS
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
+
+export default FashionDesignChapter6;
