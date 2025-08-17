0cee265d943f24e87911db20f2991030831399b2
Anri <anrigabaidze44@gmail.com>
2025-08-17 22:27:23 +0400

    feat(router): add HomePage and AuthLink routes (Just Testing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Layout/header/Header";
+import Card from "./Components/ReusableCard/Card";
 import HomePage from "./pages/Home/Home";
 
 function App() {
   return (
-    <Box
-      sx={{marginInline: "2rem"}}
-    >
-      <Box component={"header"}>
-        <Header />
-      </Box>
+    <Stack sx={{ mx: 4 }}>
+      <Header />
 
-      {/* Routes here */}
-      <HomePage />
-    </Box>
+      <Routes>
+        {/* რომელიც აღწერს თითოეულ გზას განსხვავებული გვერდების ჩვენს საიტზე */}
+        {/* path (way) is like a url that defines the location */}
+        {/* როცა გვიწერია path="/" ანუ / ეს url ნიშნავს რომ დეფოლტ ვერსია რა ფესვები ჩვენი აპლიკაციის უნდა იყოს ეს 
+        localhost:323 რაც არის მითითებული ეგაა, და აქ უნდა იყოს LandingPage */}
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/AuthLink" element={<Card />}></Route>
+      </Routes>
+    </Stack>
   );
 }
 
