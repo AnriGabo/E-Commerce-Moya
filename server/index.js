f0ca54260937f1ae9325c68bfb867670831b6b44
Anri <anrigabaidze44@gmail.com>
2025-09-08 18:35:01 +0400

    feat(server): Express API with PostgreSQL pool and dotenv; add GET "/" to return registration rows

--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-import http from 'http' 
+import { config } from "dotenv";
+config();
 
-const server = http.createServer((req,res) => {
-    res.write("Hello world i am anri gabaidze")
-    res.end();
-})
+// ბაზასთან ქონექშენი
+import pg from "pg";
+const { Pool } = pg;
 
-const PORT = 5000;
-server.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
-})
+// ვიყენებთ ექსპრესის ბიბლიოთეკას
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_database,
+  password: process.env.DB_password,
+  port: process.env.DB_PORT,
+});
+
+
+app.get("/", async (req, res) => {
+  const client = await pool.connect();
+  const result = await client.query({
+    text: "SELECT * FROM registration",
+  });
+
+  // პასუხი
+  res.json(result.rows);
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
