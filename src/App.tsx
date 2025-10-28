f787517eadb04bcce25f6f5bb68746da7aabe238
Anri <anrigabaidze44@gmail.com>
2025-10-28 23:47:58 +0400

    feat(routing): hide Footer on auth-related pages (signin, registration, access account)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Stack } from "@mui/material";
-import { Route, Routes } from "react-router-dom";
+import { Box, Stack } from "@mui/material";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import Collection from "./Components/editorial/photo-gallery/TheNewSection";
 import AccountAccessLink from "./Components/Layout/AuthLink/AccountAccessLink";
 import Footer from "./Components/Layout/Footer/Footer";
 import Header from "./Components/Layout/header/Header";
@@ -10,20 +9,25 @@ import SignIn from "./Components/Layout/signIn/SignIn";
 import HomePage from "./pages/Home/Home";
 
 function App() {
+  const location = useLocation();
+  console.log(location);
+
   return (
-    <Stack sx={{ mx: 4 }}>
+    <Stack sx={{ mx: 4, minHeight: "100vh" }}>
       <Header />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/woman" element={<Collection />} />
-        <Route path="/man" element={<Collection />} />
-        <Route path="/children" element={<Collection />} />
-        <Route path="/accsesyourAccount" element={<AccountAccessLink />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
-      <Footer />
+      <Box component={"main"} sx={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/accsesyourAccount" element={<AccountAccessLink />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Box>
+
+      {location.pathname !== "/accsesyourAccount" &&
+        location.pathname !== "/signin" &&
+        location.pathname !== "/registration" && <Footer />}
     </Stack>
   );
 }
